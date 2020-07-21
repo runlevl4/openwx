76bbbe93f63fd7208231b4ec7535a475eadb99f2
runlevl4 <root@runlevl4.com>
2020-07-20 21:40:44 -0400

    Updated documentation.

--- data/cmd/doc.go
+++ data/cmd/doc.go
@@ -1,4 +1,14 @@
 // The data folder contains the OpenWeatherAPI city list in its entirety and
-// a data process to parse the file by country, or US state.
+// a data process to parse the file by country, or US state. In order to save
+// space, you can download the actual source file here:
+// http://bulk.openweathermap.org/sample/
+//
+// Usage:
+//   -cnt string
+//   two-character country abbreviation
+// -o string
+//   output file; if not specified, will write to stdout
+// -st string
+//   two-letter US state abbreviation
 package main
 
